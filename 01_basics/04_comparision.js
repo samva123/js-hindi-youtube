0e5906277973639f4766827fc951865d002b53cc
samva123 <149029509+samva123@users.noreply.github.com>
2024-08-21 21:19:21 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,12 +5,12 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1); returns true
+// console.log("02" > 1); return true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
 
 console.log(undefined == 0);
 console.log(undefined > 0);
